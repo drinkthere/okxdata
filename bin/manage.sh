789d2d558212dc8e4a4b3fea1a86c4be4a6a78fd
Will Shi <drinkthere@gmail.com>
2024-09-26 23:41:31 +0800

    add manage.sh

---
+++ bin/manage.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+start() {
+    echo "Starting the process..."
+    nohup ./okxdata ../config/config.json > ./logs/nohup.log 2>&1 &
+    echo "Process started."
+}
+
+stop() {
+    echo "Stopping the process..."
+    pid=$(pgrep -f "okxdata ../config/config.json")
+    if [ -n "$pid" ]; then
+        kill -SIGINT $pid
+        echo "Process stopping"
+        sleep 5
+        kill $pid
+        echo "Process stopped."
+    else
+        echo "Process is not running."
+    fi
+}
+
+restart() {
+    stop
+    sleep 5
+    start
+}
+
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        restart
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
