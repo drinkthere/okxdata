548cb2646b5704c1a8af3865846b06aeacaa9f27
Will Shi <drinkthere@gmail.com>
2025-01-14 14:36:40 +0800

    fix bugs

--- bin/clearlog.sh
+++ bin/clearlog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 指定日志文件所在的目录
-log_directory="/root/code/okxdata/bin/logs"
+log_directory="/data/dc/okxdata"
 
 # 保留最近的两个okxmm.log文件
 ls -t $log_directory/okxdata.log* | tail -n +5 | xargs rm -f
