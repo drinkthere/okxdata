59c55d9c6c78d74eba2162558571ffd6902790d1
Will Shi <drinkthere@gmail.com>
2025-05-24 22:32:27 +0800

    fix bugs

--- bin/clearlog.sh
+++ bin/clearlog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 指定日志文件所在的目录
-log_directory="/data/dc/okxdata"
+log_directory="//data/dc/cold_boot_data"
 
 # 保留最近的两个okxmm.log文件
-ls -t $log_directory/okxdata.log* | tail -n +5 | xargs rm -f
+ls -t $log_directory/data.log* | tail -n +5 | xargs rm -f
 
